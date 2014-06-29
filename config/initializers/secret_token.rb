4c71a7eeef1aa7726db0fc2aa097d67e513e18e2
Diego Rodriguezg <diego@mbp-de-diego.lan>
2014-06-29 14:28:35 -0400

    primer commit :3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comparamercados::Application.config.secret_key_base = '37538ff828396880af6e85d7ad65fc854bf1a26fb80c39f2237dac7176905f913a9a1661df71b595e736e3a1b54468155c4ff37dc7e7a39341c0b3ec59e16f6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Comparamercados::Application.config.session_store :cookie_store, key: '_comparamercados_session'
